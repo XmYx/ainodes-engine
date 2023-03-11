89f5e4bfe3675bafd94cb8ce9412d92226eadcd6
Miklos Nagy <miklos.mnagy@gmail.com>
2023-03-11 23:11:22 +0100

    test_001

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 import platform
+import os
 
 with open('requirements_venv.txt') as f:
     requirements = f.read().splitlines()
 
 entry_point = 'launcher:main'
-"""if platform.system() == 'Windows':
-    entry_point = 'run.bat'
-else:
-    entry_point = 'launcher:main'"""
+
+# Get the path to the user's application data folder
+appdata = os.environ.get('APPDATA')
+if appdata is None:
+    raise RuntimeError('APPDATA environment variable not set')
+user_folder = os.path.join(appdata, 'ainodes-engine')
 
 setup(
     name='ainodes-engine',
@@ -23,5 +26,7 @@ setup(
         'console_scripts': [
             f'ainodes={entry_point}'
         ]
-    }
+    },
+    # Install the package to the user's application data folder
+    data_files=[(user_folder, ['launcher.py'])]
 )
