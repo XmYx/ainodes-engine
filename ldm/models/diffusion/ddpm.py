17a914cafc8e014f584c2955216f8cfae10d0b5f
Miklos Nagy <miklos.mnagy@gmail.com>
2023-03-24 18:30:35 +0100

    fixed loras

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -564,8 +564,8 @@ class LatentDiffusion(DDPM):
         else:
             self.register_buffer('scale_factor', torch.tensor(scale_factor))
 
-        self.instantiate_first_stage(first_stage_config)
-        self.instantiate_cond_stage(cond_stage_config)
+        #self.instantiate_first_stage(first_stage_config)
+        #self.instantiate_cond_stage(cond_stage_config)
 
         self.cond_stage_forward = cond_stage_forward
         self.clip_denoised = False
