0bba021faf74f45ed3779ee70c9fa8875c2df0f7
unknown <miklos.mnagy@gmail.com>
2023-05-19 14:30:47 +0200

    Create create_shortcut.bat

---
+++ create_shortcut.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+
+set "SCRIPT_DIR=%~dp0"
+set "APP_DIR=%SCRIPT_DIR%"
+set "TARGET_PATH=%APP_DIR%\run_skip_update.bat"
+set "ICON_PATH=%APP_DIR%\ainodes_frontend\qss\icon.ico"
+set "SHORTCUT_NAME=%USERPROFILE%\Desktop\aiNodes - Engine.lnk"
+
+set "VBS_SCRIPT=%TEMP%\CreateShortcut.vbs"
+>"%VBS_SCRIPT%" (
+    echo Set oWS = WScript.CreateObject("WScript.Shell"^)
+    echo sLinkFile = "%SHORTCUT_NAME%"
+    echo Set oLink = oWS.CreateShortcut(sLinkFile^)
+    echo oLink.TargetPath = "%TARGET_PATH%"
+    echo oLink.IconLocation = "%ICON_PATH%"
+    echo oLink.WorkingDirectory = "%APP_DIR%"
+    echo oLink.Save
+)
+
+cscript //nologo "%VBS_SCRIPT%"
+del "%VBS_SCRIPT%"
+
+endlocal
