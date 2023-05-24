22c4f9689b093cb346bd94e73d728bd88020404c
unknown <miklos.mnagy@gmail.com>
2023-05-24 11:45:15 +0200

    Update clean-install.bat

--- clean-install.bat
+++ clean-install.bat
@@ -1,4 +1,27 @@
 @echo off
 if exist nodes_env rmdir /s /q nodes_env
-if exist custom_nodes\ainodes_engine_base_nodes rmdir /s /q custom_nodes\ainodes_engine_base_nodes
+
+@echo off
+setlocal enabledelayedexpansion
+
+set "repositories_file=repositories.txt"
+set "custom_nodes_folder=custom_nodes"
+
+rem Read repositories from repositories.txt
+for /f "tokens=1 delims=/" %%A in (%repositories_file%) do (
+  set "repository=%%A"
+
+  rem Remove folder corresponding to repository
+  set "folder_name=!repository:_=!"
+  echo Removing folder: %custom_nodes_folder%\!folder_name!
+  if exist %custom_nodes_folder%\!folder_name! (
+    rmdir /s /q %custom_nodes_folder%\!folder_name!
+  )
+)
+
+endlocal
+
+call download_node_packs.bat
+call create_shortcut.bat
+
 python launcher.py
