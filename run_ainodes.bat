74139f5664980136eb2ac96b49979f2b6ab4efd6
unknown <miklos.mnagy@gmail.com>
2023-05-29 01:01:43 +0200

    Update run_ainodes.bat

--- run_ainodes.bat
+++ run_ainodes.bat
@@ -5,10 +5,16 @@ set "SCRIPT_DIR=%~dp0"
 set "APP_DIR=%SCRIPT_DIR%"
 set "PYTHON_SCRIPT=%APP_DIR%\main.py"
 
+set "PYTHON_DIR=%SCRIPT_DIR%src\python"
+set "PYTHON_LIB_DIR=%PYTHON_DIR%\Lib"
+set "PYTHON_SCRIPTS_DIR=%PYTHON_DIR%\Scripts"
+set "PATH=%PYTHON_SCRIPTS_DIR%;%PYTHON_LIB_DIR%;%PYTHON_DIR%;%PATH%"
+
+
 set "VBS_SCRIPT=%TEMP%\RunPythonScript.vbs"
 >"%VBS_SCRIPT%" (
     echo Set oWS = CreateObject("WScript.Shell"^)
-    echo oWS.Run "cmd /C nodes_env/Scripts/activate.bat & python.exe ""%PYTHON_SCRIPT%""", 0
+    echo oWS.Run "cmd /C %SCRIPT_DIR%nodes_env/Scripts/activate.bat & %SCRIPT_DIR%nodes_env/Scripts/python.exe ""%PYTHON_SCRIPT%""", 0
 )
 
 cscript //nologo "%VBS_SCRIPT%"
