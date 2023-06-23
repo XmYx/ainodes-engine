5264538fa713b99e5fef2c0b4702575ff7e9e258
unknown <miklos.mnagy@gmail.com>
2023-06-23 23:04:52 +0200

    comfy adapter update

--- ainodes_frontend/node_engine/node_content_widget.py
+++ ainodes_frontend/node_engine/node_content_widget.py
@@ -351,7 +351,7 @@ class QDMNodeContentWidget(QWidget, Serializable):
         self.widget_list.append(label)
         return label
 
-    def create_spin_box(self, label_text, min_val, max_val, default_val, step_value=1, accessible_name=None) -> QtWidgets.QSpinBox:
+    def create_spin_box(self, label_text, min_val, max_val, default_val, step=1, accessible_name=None) -> QtWidgets.QSpinBox:
         """Create a spin box widget with the given label text, minimum value, maximum value, default value, and step value.
 
         Args:
@@ -359,7 +359,7 @@ class QDMNodeContentWidget(QWidget, Serializable):
             min_val (int): Minimum value of the spin box.
             max_val (int): Maximum value of the spin box.
             default_val (int): Default value of the spin box.
-            step_value (int, optional): Step value of the spin box. Defaults to 1.
+            step (int, optional): Step value of the spin box. Defaults to 1.
 
         Returns:
             QtWidgets.QSpinBox: A spin box widget.
@@ -368,7 +368,7 @@ class QDMNodeContentWidget(QWidget, Serializable):
         spin_box.setMinimum(min_val)
         spin_box.setMaximum(max_val)
         spin_box.setValue(default_val)
-        spin_box.setSingleStep(step_value)
+        spin_box.setSingleStep(step)
         spin_box.setObjectName(label_text)
         if accessible_name is not None:
             spin_box.setAccessibleName(accessible_name)
