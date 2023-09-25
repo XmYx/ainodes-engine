89602d6788f7bd28c69a0efd43c29172a4aa5232
Miklos Nagy <miklos.mnagy@gmail.com>
2023-09-25 19:21:10 +0200

    setup

--- setup_ainodes.bat
+++ setup_ainodes.bat
@@ -1,51 +1,60 @@
-REM Set variables
-set "PYTHON_ZIP_URL=https://www.python.org/ftp/python/3.10.10/python-3.10.10-embed-amd64.zip"
-set "PYTHON_ZIP_FILE=%~dp0src\python\python-3.10.10-embed-amd64.zip"
-set "PYTHON_EXTRACT_PATH=%~dp0src\python"
-REM Get the full path of the script directory
-set "SCRIPT_DIR=%~dp0"
-REM Construct the full path to the Python Scripts directory
-set "PYTHON_DIR=%SCRIPT_DIR%src\python"
-set "SRC_DIR=%SCRIPT_DIR%src"
-set "PYTHON_LIB_DIR=%PYTHON_DIR%\Lib"
-set "PYTHON_SCRIPTS_DIR=%PYTHON_DIR%\Scripts"
-set "BACKUPPATH=%PATH%"
+@echo off
 
-REM Check if Python executable exists
-if not exist "%PYTHON_EXTRACT_PATH%\python.exe" (
-    echo Python executable not found. Downloading Python embeddable package...
-    mkdir "%PYTHON_EXTRACT_PATH%"
-    curl -o "%PYTHON_ZIP_FILE%" "%PYTHON_ZIP_URL%"
-    echo Extracting Python embeddable package...
-    powershell -Command "Expand-Archive -Path '%PYTHON_ZIP_FILE%' -DestinationPath '%PYTHON_EXTRACT_PATH%' -Force"
+cd /D "%~dp0"
+
+set PATH=%PATH%;%SystemRoot%\system32
+
+echo "%CD%"| findstr /C:" " >nul && echo This script relies on Miniconda which can not be silently installed under a path with spaces. && goto end
+
+@rem Check for special characters in installation path
+set "SPCHARMESSAGE="WARNING: Special characters were detected in the installation path!" "         This can cause the installation to fail!""
+echo "%CD%"| findstr /R /C:"[!#\$%&()\*+,;<=>?@\[\]\^`{|}~]" >nul && (
+	call :PrintBigMessage %SPCHARMESSAGE%
+)
+set SPCHARMESSAGE=
+
+@rem Fix failed install when installing to a separate drive
+set TMP=%cd%\installer_files
+set TEMP=%cd%\installer_files
+
+@rem Deactivate existing conda envs as needed to avoid conflicts
+(call conda deactivate && call conda deactivate && call conda deactivate) 2>nul
+
+@rem Configuration
+set INSTALL_DIR=%cd%\installer_files
+set CONDA_ROOT_PREFIX=%cd%\installer_files\conda
+set INSTALL_ENV_DIR=%cd%\nodes_env
+
+set INSTALL_ENV_NAME=nodes_env
+set MINICONDA_DOWNLOAD_URL=https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Windows-x86_64.exe
+set conda_exists=F
+
+@rem Check if conda is already installed
+call "%CONDA_ROOT_PREFIX%\_conda.exe" --version >nul 2>&1
+if "%ERRORLEVEL%" EQU "0" set conda_exists=T
+
+@rem Download and install Miniconda if it's not installed
+if "%conda_exists%" == "F" (
+	echo Downloading Miniconda from %MINICONDA_DOWNLOAD_URL% to %INSTALL_DIR%\miniconda_installer.exe
+
+	mkdir "%INSTALL_DIR%"
+	call curl -Lk "%MINICONDA_DOWNLOAD_URL%" > "%INSTALL_DIR%\miniconda_installer.exe" || ( echo. && echo Miniconda failed to download. && goto end )
+
+	echo Installing Miniconda to %CONDA_ROOT_PREFIX%
+	start /wait "" "%INSTALL_DIR%\miniconda_installer.exe" /InstallationType=JustMe /NoShortcuts=1 /AddToPath=0 /RegisterPython=0 /NoRegistry=1 /S /D=%CONDA_ROOT_PREFIX%
+
+	@rem Test the conda binary
+	echo Miniconda version:
+	call "%CONDA_ROOT_PREFIX%\_conda.exe" --version || ( echo. && echo Miniconda not found. && goto end )
 )
 
-move "%PYTHON_EXTRACT_PATH%\python310._pth" "%PYTHON_EXTRACT_PATH%\python310_pth.bak"
-copy "%SCRIPT_DIR%config\default_python310._pth" "%PYTHON_EXTRACT_PATH%\python310._pth"
-
-
-REM Update the PATH environment variable
-set "PATH=%PYTHON_SCRIPTS_DIR%;%PYTHON_LIB_DIR%;%PYTHON_DIR%"
-
-REM Install pip
-if not exist "%PYTHON_SCRIPTS_DIR%\pip.exe" (
-	call "%PYTHON_DIR%\python.exe" "%SCRIPT_DIR%get-pip.py"
+@rem create the installer env
+if not exist "%INSTALL_ENV_DIR%" (
+	call "%CONDA_ROOT_PREFIX%\_conda.exe" create --no-shortcuts -y -k --prefix "%INSTALL_ENV_DIR%" python=3.10 || ( echo. && echo Conda environment creation failed. && goto end )
 )
 
-
-REM Install virtualenv using pip
-if not exist "%SCRIPT_DIR%nodes_env" (
-	pip install virtualenv
-	REM Create virtual environment
-	%PYTHON_DIR%\python.exe -m virtualenv nodes_env
-)
-
-
-REM Activate the virtual environment
-call "%SCRIPT_DIR%nodes_env\Scripts\activate.bat"
-
-REM Restore the original PATH environment variable
-set "PATH=%PATH%;%BACKUPPATH%"
+@rem activate installer env
+call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%" || ( echo. && echo Miniconda hook not found. && goto end )
 
 REM Install requirements
 pip install -r requirements.txt
