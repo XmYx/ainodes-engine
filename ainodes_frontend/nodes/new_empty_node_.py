bd8d2da7b82a83129473f300b69733391fadafa7
Miklos Nagy <miklos.mnagy@gmail.com>
2024-01-12 21:31:30 +0100

    Update new_empty_node_.py

--- ainodes_frontend/nodes/new_empty_node_.py
+++ ainodes_frontend/nodes/new_empty_node_.py
@@ -3,7 +3,20 @@ from ainodes_frontend.base import register_node, get_next_opcode
 from ainodes_frontend.base import AiNode, CalcGraphicsNode
 from ainodes_frontend.base.qimage_ops import pixmap_to_tensor, tensor_image_to_pixmap, tensor2pil, pil2tensor
 from ainodes_frontend.node_engine.node_content_widget import QDMNodeContentWidget
+from backend_helpers.torch_helpers.torch_gc import torch_gc
 
+
+
+"""
+When creating a new node, make sure to change the global OP_NODE_UNIQUE to something unique, like OP_NODE_MYVALUE = get_next_opcode(),
+then replace in your node class as well. The next steps are renaming your CustomWidget, your CustomNode, then setting the
+content_labale_objname to something unique. Feel free to add inputs, outputs, and any custom function inside the evalImplementation_thread().
+
+Use the built in torch_gc function to clear some VRAM, and don't forget to unload your model in the remove function if any.
+
+There are also other methods available: tensor2pil, pil2tensor
+
+"""
 OP_NODE_UNIQUE = get_next_opcode()
 
 from ainodes_frontend import singleton as gs
@@ -37,9 +50,40 @@ class CustomNode(AiNode):
     category = "custom/category"
     make_dirty = True
     help_text = "Help text to appear"
-    custom_input_socket_name = ["IMG_0", "EXEC"]
+    # custom_input_socket_name = ["IMG_0", "EXEC"]
+    # custom_output_socket_name = ["IMG_0", "EXEC"]
 
     def __init__(self, scene):
+
+        """
+         Initialize a CustomNode class with a scene, inputs, and outputs.
+
+         Args:
+             scene (QGraphicsScene): The scene where the node is displayed.
+             inputs (list): A list of input sockets, with values representing their types.
+                           1: EXEC
+                           2: LATENT
+                           3: CONDITIONING
+                           4: PIPE/MODEL
+                           5: IMAGE
+                           6: DATA
+                           7: STRING
+                           8: INT
+                           9: FLOAT
+                           Defaults to [2,2].
+             outputs (list): A list of output sockets, with values representing their types.
+                           1: EXEC
+                           2: LATENT
+                           3: CONDITIONING
+                           4: PIPE/MODEL
+                           5: IMAGE
+                           6: DATA
+                           7: STRING
+                           8: INT
+                           9: FLOAT
+                           Defaults to [1].
+         """
+
         super().__init__(scene, inputs=[5,1], outputs=[5,1])
 
     def evalImplementation_thread(self):
