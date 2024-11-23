1db90610b92e891e2ccc38e9ea4ab688319c8619
XmYx <miklos.mnagy@gmail.com>
2024-11-23 22:03:51 +0100

    refactor_06
    Comfy to aiNodes json converter

--- src/ainodes/nodeeditor/node_editor_window.py
+++ src/ainodes/nodeeditor/node_editor_window.py
@@ -68,6 +68,7 @@ class NodeEditorWindow(QMainWindow):
         self.actCopy = QAction('&Copy', self, shortcut='Ctrl+C', statusTip="Copy to clipboard", triggered=self.onEditCopy)
         self.actPaste = QAction('&Paste', self, shortcut='Ctrl+V', statusTip="Paste from clipboard", triggered=self.onEditPaste)
         self.actDelete = QAction('&Delete', self, shortcut='Del', statusTip="Delete selected items", triggered=self.onEditDelete)
+        self.actOpenComfyWorkflow = QAction('Open Comfy Workflow', self, statusTip='Open Comfy Workflow', triggered=self.onOpenComfyWorkflow)
 
 
     def createMenus(self):
@@ -81,6 +82,7 @@ class NodeEditorWindow(QMainWindow):
         self.fileMenu.addAction(self.actNew)
         self.fileMenu.addSeparator()
         self.fileMenu.addAction(self.actOpen)
+        self.fileMenu.addAction(self.actOpenComfyWorkflow)
         self.fileMenu.addAction(self.actSave)
         self.fileMenu.addAction(self.actSaveAs)
         self.fileMenu.addSeparator()
@@ -283,3 +285,169 @@ class NodeEditorWindow(QMainWindow):
         settings = QSettings(self.name_company, self.name_product)
         settings.setValue('pos', self.pos())
         settings.setValue('size', self.size())
+    # New method to handle opening a Comfy workflow
+    def onOpenComfyWorkflow(self):
+        """Handle the action of opening a Comfy workflow, converting it to Ainodes format, and loading it."""
+        if self.maybeSave():
+            fname, _ = QFileDialog.getOpenFileName(
+                self,
+                'Open Comfy Workflow',
+                self.getFileDialogDirectory(),
+                'Comfy Workflow (*.json);;All files (*)'
+            )
+            if fname != '' and os.path.isfile(fname):
+                # Read the Comfy JSON file
+                with open(fname, 'r') as f:
+                    comfy_data = json.load(f)
+                # Convert Comfy JSON to Ainodes JSON
+                ainodes_data = self.convertComfyToAinodes(comfy_data)
+                # Load Ainodes JSON into the node editor
+                with open('temp.json', "w") as file:
+                    file.write(json.dumps(ainodes_data, indent=4))
+                self.getCurrentNodeEditorWidget().fileLoad('temp.json')
+                self.setTitle()
+
+    def convertComfyToAinodes(self, comfy_data):
+        """Convert Comfy JSON data to Ainodes JSON format.
+
+        :param comfy_data: Dictionary loaded from Comfy JSON
+        :return: Ainodes data dictionary
+        """
+        # Initialize the Ainodes data dictionary
+        ainodes_data = {
+            "id": 1,
+            "scene_width": 64000,
+            "scene_height": 64000,
+            "nodes": [],
+            "edges": []
+        }
+
+        node_id_map = {}
+        socket_id_map = {}
+        next_node_id = 1
+        next_socket_id = 1000
+
+        # Mapping from Comfy types to Ainodes socket_type
+        type_to_socket_type = {
+            "EXEC": 1,
+            "LATENT": 2,
+            "CONDITIONING": 3,
+            "PIPE": 4,
+            "MODEL": 4,
+            "VAE": 4,
+            "CLIP": 4,
+            "COGVIDEOPIPE": 4,
+            "IMAGE": 5,
+            "DATA": 6,
+            "STRING": 7,
+            "INT": 8,
+            "FLOAT": 9,
+            # Any other types default to 7 (STRING)
+        }
+
+        # Process nodes
+        for comfy_node in comfy_data['nodes']:
+            comfy_node_id = comfy_node['id']
+            ainodes_node_id = next_node_id
+            node_id_map[comfy_node_id] = ainodes_node_id
+            next_node_id += 1
+
+            # Get position
+            pos_dict = comfy_node['pos']
+            pos_x = pos_dict.get("0", 0)
+            pos_y = pos_dict.get("1", 0)
+
+            ainodes_node = {
+                "id": ainodes_node_id,
+                "title": comfy_node['type'],
+                "pos_x": pos_x,
+                "pos_y": pos_y,
+                "inputs": [],
+                "outputs": [],
+                "content": {},
+                "op_code": 0,
+                "content_label_objname": comfy_node['type'].lower()
+            }
+
+            custom_input_socket_names = []
+            custom_output_socket_names = []
+            input_socket_types = []
+            output_socket_types = []
+
+            # Process inputs
+            for input_socket in comfy_node.get('inputs', []):
+                socket_id = next_socket_id
+                next_socket_id += 1
+                comfy_socket_type = input_socket['type']
+                socket_type = type_to_socket_type.get(comfy_socket_type, 7)  # default to 7 (STRING)
+                input_dict = {
+                    "id": socket_id,
+                    "index": input_socket.get('slot_index', 0),
+                    "multi_edges": False,
+                    "position": 3,
+                    "socket_type": socket_type,
+                    "name": input_socket['name']
+                }
+                socket_id_map[(comfy_node_id, 'input', input_socket.get('slot_index', 0))] = socket_id
+                ainodes_node['inputs'].append(input_dict)
+
+                custom_input_socket_names.append(input_socket['name'])
+                input_socket_types.append(socket_type)
+
+            # Process outputs
+            for output_socket in comfy_node.get('outputs', []):
+                socket_id = next_socket_id
+                next_socket_id += 1
+                comfy_socket_type = output_socket['type']
+                socket_type = type_to_socket_type.get(comfy_socket_type, 7)  # default to 7 (STRING)
+                output_dict = {
+                    "id": socket_id,
+                    "index": output_socket.get('slot_index', 0),
+                    "multi_edges": True,
+                    "position": 6,
+                    "socket_type": socket_type,
+                    "name": output_socket['name']
+                }
+                socket_id_map[(comfy_node_id, 'output', output_socket.get('slot_index', 0))] = socket_id
+                ainodes_node['outputs'].append(output_dict)
+
+                custom_output_socket_names.append(output_socket['name'])
+                output_socket_types.append(socket_type)
+
+            # Map content (properties and widgets_values)
+            content = {}
+            content['properties'] = comfy_node.get('properties', {})
+            content['widgets_values'] = comfy_node.get('widgets_values', [])
+            content['custom_input_socket_names'] = custom_input_socket_names
+            content['custom_output_socket_names'] = custom_output_socket_names
+            content['_inputs'] = input_socket_types
+            content['_outputs'] = output_socket_types
+            # Include other node parameters as needed
+
+            ainodes_node['content'] = content
+
+            ainodes_data['nodes'].append(ainodes_node)
+
+        # Process links
+        for link in comfy_data.get('links', []):
+            # Link format: [link_id, from_node_id, from_socket_index, to_node_id, to_socket_index, "TYPE"]
+            if len(link) < 6:
+                continue  # Skip invalid links
+            link_id, from_node_id, from_socket_index, to_node_id, to_socket_index, _ = link
+
+            from_socket_id = socket_id_map.get((from_node_id, 'output', from_socket_index))
+            to_socket_id = socket_id_map.get((to_node_id, 'input', to_socket_index))
+
+            if from_socket_id is None or to_socket_id is None:
+                continue  # Skip if sockets are not found
+
+            ainodes_edge = {
+                "id": link_id,
+                "edge_type": 5,
+                "start": from_socket_id,
+                "end": to_socket_id
+            }
+
+            ainodes_data['edges'].append(ainodes_edge)
+
+        return ainodes_data
